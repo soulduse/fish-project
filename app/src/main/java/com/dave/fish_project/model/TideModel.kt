e25d0e9567ffc76372edaf6b9a4722c5bbe2b0bb
soulduse <soulduse@naver.com>
2017-08-23 01:52:31 +0900

    Model class 생

---
+++ app/src/main/java/com/dave/fish_project/model/TideModel.kt
@@ -0,0 +1,46 @@
+package com.dave.fish_project.model
+
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+/**
+ * Created by soul on 2017. 8. 21..
+ */
+class TideModel {
+
+    @SerializedName("weeklyDataDTO")
+    var weeklyDataDTO :WeeklyDataDTO ?= null
+
+    @SerializedName("weeklyData")
+    var weeklyDataList :List<WeeklyData> ?= null
+
+    data class WeeklyDataDTO(
+            var obsPostId : String,
+            var stDate : Date,
+            var enDate : Date,
+            var year : String,
+            var addDays : String,
+            var lunarCode : String
+    )
+
+    data class WeeklyData(
+            var searchDate:Date,
+            var obsPostName:String,
+            var obsLat:String,
+            var obsLon:String,
+            var lvl1:String,
+            var lvl2:String,
+            var lvl3:String,
+            var lvl4:String,
+            var flgView:String,
+            var dateSun:Date,
+            var dateMoon:Date,
+            var moonState:String,
+            var moolNormal:String,
+            var mool7:String,
+            var mool8:String,
+            var upTime:String,
+            var downTime:String,
+            var temp:String
+    )
+}
