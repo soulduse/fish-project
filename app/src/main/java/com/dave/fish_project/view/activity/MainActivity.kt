5d1cb4f3a2480454c6bb0bd56de97c5656960e82
soulduse <soulduse@naver.com>
2017-09-02 14:12:08 +0900

    캘린더 라이브러리 적용 및 UI 구성

--- app/src/main/java/com/dave/fish_project/view/activity/MainActivity.kt
+++ app/src/main/java/com/dave/fish_project/view/activity/MainActivity.kt
@@ -1,42 +1,26 @@
 package com.dave.fish_project.view.activity
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.dave.fish_project.R
 import com.dave.fish_project.view.adapter.ViewPagerAdapter
 import com.dave.fish_project.view.fragment.FragmentMenuOne
 import com.dave.fish_project.view.fragment.FragmentMenuTwo
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
-        menu ->
-        when(menu.itemId){
-            R.id.navigation_home -> {
-                Log.d(TAG, "navigation home")
-                main_viewpager.currentItem = 0
-                true
-            }
-            R.id.navigation_reserved ->{
-                Log.d(TAG, "navigation reserved")
-                main_viewpager.currentItem = 1
-                true
-            }
-            else -> false
-        }
-    }
+    private val tabIcons = intArrayOf(
+            R.drawable.ic_date_range_white_24dp, R.drawable.ic_cloud_white_24dp, R.drawable.ic_toys_white_24dp)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        navigation_menu.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         setupViewPager()
+        tabs.setupWithViewPager(main_viewpager)
+        setupTabIcons()
     }
 
     private fun setupViewPager(){
@@ -44,20 +28,12 @@ class MainActivity : AppCompatActivity() {
             addFragment(FragmentMenuOne())
             addFragment(FragmentMenuTwo())
         }
+    }
 
-        main_viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
-            override fun onPageScrollStateChanged(state: Int) {
-
-            }
-
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-
-            }
-
-            override fun onPageSelected(position: Int) {
-                navigation_menu.menu.getItem(position).isChecked = true
-            }
-        })
+    private fun setupTabIcons() {
+        tabs.getTabAt(0)?.icon = resources.getDrawable(tabIcons[0])
+        tabs.getTabAt(1)?.icon = resources.getDrawable(tabIcons[1])
+        tabs.getTabAt(2)?.icon = resources.getDrawable(tabIcons[2])
     }
 
     companion object {
--- app/src/main/java/com/dave/fish_project/view/fragment/FragmentMenuOne.kt
+++ app/src/main/java/com/dave/fish_project/view/fragment/FragmentMenuOne.kt
@@ -9,6 +9,9 @@ import android.view.ViewGroup
 import com.dave.fish_project.R
 import com.dave.fish_project.model.WeeklyModel
 import com.dave.fish_project.network.RetrofitController
+import com.sickmartian.calendarview.CalendarView
+import kotlinx.android.synthetic.main.fragment_menu_one.*
+import java.util.*
 
 /**
  * Created by soul on 2017. 8. 27..
@@ -16,15 +19,45 @@ import com.dave.fish_project.network.RetrofitController
 class FragmentMenuOne : Fragment(){
 
     var tideData : WeeklyModel?= null
+    var firstDayOfWeek : Int ?= null
+
+    private val DAY_PARAMETER = "day"
+    private val MONTH_PARAMETER = "month"
+    private val YEAR_PARAMETER = "year"
+    private val FIRST_DAY_OF_WEEK_PARAMETER = "firstDay"
+
+    private var mYear: Int = 0
+    private var mDay: Int = 0
+    private var mMonth: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initData()
+//        initData()
+        firstDayOfWeek = CalendarView.MONDAY_SHIFT
+        if (savedInstanceState == null) {
+            val cal = Calendar.getInstance()
+            setStateByCalendar(cal)
+        } else {
+            mDay = savedInstanceState.getInt(DAY_PARAMETER)
+            mMonth = savedInstanceState.getInt(MONTH_PARAMETER)
+            mYear = savedInstanceState.getInt(YEAR_PARAMETER)
+            firstDayOfWeek = savedInstanceState.getInt(FIRST_DAY_OF_WEEK_PARAMETER)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // The two views can't have the same id, or the state won't be preserved
+        // correctly and they will throw an exception
 
         return inflater?.inflate(R.layout.fragment_menu_one, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setDateByStateDependingOnView()
+        monthView.firstDayOfTheWeek = CalendarView.MONDAY_SHIFT
+        monthView.setCurrentDay(getCalendarForState())
     }
 
     private fun initData(){
@@ -33,7 +66,6 @@ class FragmentMenuOne : Fragment(){
                     tideModel->
                     var weeklyDataList = tideModel.weeklyDataList
                     for(item : WeeklyModel.WeeklyData in weeklyDataList!!){
-
                     }
                     Log.d(TAG, "used api")
                 }, {
@@ -42,6 +74,38 @@ class FragmentMenuOne : Fragment(){
                 })
     }
 
+    fun setStateByCalendar(cal: Calendar) {
+        mYear = cal.get(Calendar.YEAR)
+        mMonth = cal.get(Calendar.MONTH) + 1 // We use base 1 months..
+        // You should use joda time or a sane Calendar really
+        mDay = cal.get(Calendar.DATE)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(DAY_PARAMETER, mDay)
+        outState.putInt(MONTH_PARAMETER, mMonth)
+        outState.putInt(YEAR_PARAMETER, mYear)
+        outState.putInt(FIRST_DAY_OF_WEEK_PARAMETER, monthView.firstDayOfTheWeek)
+    }
+
+    private fun getCalendarForState(): Calendar {
+        val newCalendar = Calendar.getInstance()
+        newCalendar.minimalDaysInFirstWeek = 1
+        newCalendar.firstDayOfWeek = Calendar.SUNDAY
+        newCalendar.set(Calendar.YEAR, mYear)
+        newCalendar.set(Calendar.MONTH, mMonth - 1)
+        newCalendar.set(Calendar.DATE, mDay)
+        newCalendar.set(Calendar.HOUR_OF_DAY, 0)
+        newCalendar.set(Calendar.MINUTE, 0)
+        newCalendar.set(Calendar.SECOND, 0)
+        newCalendar.set(Calendar.MILLISECOND, 0)
+        return newCalendar
+    }
+
+    private fun setDateByStateDependingOnView() {
+        monthView.setDate(mMonth, mYear)
+    }
 
     companion object {
         private val TAG = FragmentMenuOne.javaClass.simpleName
