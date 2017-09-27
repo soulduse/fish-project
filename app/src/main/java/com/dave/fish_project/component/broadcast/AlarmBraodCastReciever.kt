b5e2434e4ec26c75f5040831fd73b120b09bdadb
soulduse <soulduse@naver.com>
2017-09-27 22:24:07 +0900

    알람등록을 위해 브로드캐스트 생

---
+++ app/src/main/java/com/dave/fish_project/component/broadcast/AlarmBraodCastReciever.kt
@@ -0,0 +1,14 @@
+package com.dave.fish_project.component.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Created by soul on 2017. 9. 27..
+ */
+class AlarmBraodCastReciever : BroadcastReceiver() {
+    override fun onReceive(context : Context, intent:Intent){
+
+    }
+}
