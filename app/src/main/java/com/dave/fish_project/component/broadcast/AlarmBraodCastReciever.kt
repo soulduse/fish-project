cb6b1a4e422f8ad3427bfba5415c30c459ed5ce6
soulduse <soulduse@naver.com>
2017-09-28 23:54:06 +0900

    add alarmManager - 알람시계 기능 구현필

--- app/src/main/java/com/dave/fish_project/component/broadcast/AlarmBraodCastReciever.kt
+++ app/src/main/java/com/dave/fish_project/component/broadcast/AlarmBraodCastReciever.kt
@@ -1,14 +1,32 @@
 package com.dave.fish_project.component.broadcast
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import org.joda.time.DateTime
 
 /**
  * Created by soul on 2017. 9. 27..
  */
 class AlarmBraodCastReciever : BroadcastReceiver() {
-    override fun onReceive(context : Context, intent:Intent){
+
+    private lateinit var alarmMgr: AlarmManager
+    private lateinit var alarmIntent: PendingIntent
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        // val intent = Intent(context, AlarmReceiver::java.class)
+        alarmMgr = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+
+        var dateTime = DateTime()
+
+        // setRepeating() lets you specify a precise custom interval--in this case,
+        // 20 minutes.
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, dateTime.millis,
+                1000 * 60 * 20, alarmIntent)
 
     }
 }
