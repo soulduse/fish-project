f8c217389a800e4850e7c958dba8dfe053c90ed7
soulduse <soulduse@naver.com>
2017-09-29 20:29:37 +0900

    알람용 브로드캐스트 리시버, 서비스 구현

--- app/src/main/java/com/dave/fish_project/component/broadcast/AlarmBraodCastReciever.kt
+++ app/src/main/java/com/dave/fish_project/component/broadcast/AlarmBraodCastReciever.kt
@@ -5,6 +5,8 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import com.dave.fish_project.component.service.PersistentService
 import org.joda.time.DateTime
 
 /**
@@ -28,5 +30,33 @@ class AlarmBraodCastReciever : BroadcastReceiver() {
         alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, dateTime.millis,
                 1000 * 60 * 20, alarmIntent)
 
+
+        Log.i(TAG, "000 RestartService RestartService called : " + intent.action)
+
+        /**
+         * 서비스 죽일때 알람으로 다시 서비스 등록
+         */
+        if (intent.action == "ACTION.RESTART.PersistentService") {
+
+            Log.i(TAG, "000 RestartService ACTION.RESTART.PersistentService ")
+
+            val i = Intent(context, PersistentService::class.java)
+            context.startService(i)
+        }
+
+        /**
+         * 폰 재시작 할때 서비스 등록
+         */
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+
+            Log.i(TAG, "ACTION_BOOT_COMPLETED")
+            val i = Intent(context, PersistentService::class.java)
+            context.startService(i)
+
+        }
+    }
+
+    companion object {
+        private val TAG = AlarmBraodCastReciever::class.java.simpleName
     }
 }
