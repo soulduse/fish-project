132b3c57d938faa738bcd3962784679042d5ef1b
soulduse <soulduse@naver.com>
2017-10-07 22:43:25 +0900

    패키지, 프로가드, tide_detail view 정

--- app/src/main/java/com/dave/fish/view/activity/TideDetailActivity.kt
+++ app/src/main/java/com/dave/fish/view/activity/TideDetailActivity.kt
@@ -15,85 +15,80 @@ import kotlinx.android.synthetic.main.activity_tide_detail.*
  * Created by soul on 2017. 10. 3..
  */
 class TideDetailActivity : AppCompatActivity() {
+    private val realm: Realm = Realm.getDefaultInstance()
 
-    private val realm : Realm = Realm.getDefaultInstance()
-    private val mRealmController : RealmController = RealmController.instance
-    private var tideWeeklyItem : TideWeeklyModel = TideWeeklyModel()
-
+    private val mRealmController: RealmController = RealmController.instance
+    private var tideWeeklyItem: TideWeeklyModel = TideWeeklyModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tide_detail)
         init()
     }
 
-    private fun init(){
+    private fun init() {
         initData()
         initLayout()
     }
 
-    private fun initLayout(){
+    private fun initLayout() {
         initToolbar()
 
         val highList = TideUtil.getHighItemList()
         val lowList = TideUtil.getLowItemList()
 
-        for(i in 0 .. highList.size){
-            when(i){
-                0 ->{
-                    val tideTime = TideUtil.getTime(highList[i])
-                    val tideHeight = TideUtil.getHeight(highList[i])
-                    lvl1_time.text = tideTime
-                    lvl1_height.text = tideHeight
+        for (i in 0..highList.size) {
+            when (i) {
+                0 -> {
+                    lvl1_time.text = TideUtil.getTime(highList[i])
+                    lvl1_height.text = TideUtil.getHeight(highList[i])
                 }
 
-                1 ->{
-                    val tideTime = TideUtil.getTime(highList[i])
-                    val tideHeight = TideUtil.getHeight(highList[i])
-                    lvl3_time.text = tideTime
-                    lvl3_height.text = tideHeight
+                1 -> {
+                    try {
+                        lvl3_time.text = TideUtil.getTime(highList[i])
+                        lvl3_height.text = TideUtil.getHeight(highList[i])
+                    } catch (e: IndexOutOfBoundsException) {
+                        lvl3_time.text = resources.getText(R.string.no_data_time)
+                        lvl3_height.text = resources.getText(R.string.no_data_height)
+                    }
                 }
             }
         }
 
-        for(i in 0 .. lowList.size){
-            when(i){
-                0 ->{
-                    val tideTime = TideUtil.getTime(lowList[i])
-                    val tideHeight = TideUtil.getHeight(lowList[i])
-                    lvl2_time.text = tideTime
-                    lvl2_height.text = tideHeight
+        for (i in 0..lowList.size) {
+            when (i) {
+                0 -> {
+                    lvl2_time.text = TideUtil.getTime(lowList[i])
+                    lvl2_height.text = TideUtil.getHeight(lowList[i])
                 }
 
-                1 ->{
-                    try{
-                        val tideTime = TideUtil.getTime(lowList[i])
-                        val tideHeight = TideUtil.getHeight(lowList[i])
-                        lvl4_time.text = tideTime
-                        lvl4_height.text = tideHeight
-                    }catch (e:IndexOutOfBoundsException){
+                1 -> {
+                    try {
+                        lvl4_time.text = TideUtil.getTime(lowList[i])
+                        lvl4_height.text = TideUtil.getHeight(lowList[i])
+                    } catch (e: IndexOutOfBoundsException) {
                         lvl4_time.text = resources.getText(R.string.no_data_time)
                         lvl4_height.text = resources.getText(R.string.no_data_height)
-                        lvl4_down_img.setBackgroundColor(R.drawable.graph_no_data)
                     }
                 }
             }
         }
     }
 
-    private fun initData(){
+    private fun initData() {
         val selectedPostName = mRealmController.findSelectedSpinnerItem(realm)?.postName
         val selectedDate = intent.getStringExtra(Global.INTENT_DATE)
-        val key = selectedPostName+"_"+selectedDate
+        val key = selectedPostName + "_" + selectedDate
         Log.w(TAG, "What is key --> $key")
 
         tideWeeklyItem = mRealmController.findTideWeekly(realm, key)
-        Log.w(TAG, "tideWeeklyItem --> "+tideWeeklyItem.toString())
+        Log.w(TAG, "tideWeeklyItem --> " + tideWeeklyItem.toString())
         TideUtil.setTide(tideWeeklyItem)
     }
 
-    private fun initToolbar(){
+    private fun initToolbar() {
         setSupportActionBar(toolbar)
-        toolbar_title.text = tideWeeklyItem.obsPostName+" "+tideWeeklyItem.dateSun
+        toolbar_title.text = tideWeeklyItem.obsPostName + " " + tideWeeklyItem.dateSun
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
