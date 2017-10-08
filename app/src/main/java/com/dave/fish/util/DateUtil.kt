2d0f95a2d04b906c44482efc363bf579a5cce825
soulduse <soulduse@naver.com>
2017-10-09 00:09:04 +0900

    DateUtill 내용 추

--- app/src/main/java/com/dave/fish/util/DateUtil.kt
+++ app/src/main/java/com/dave/fish/util/DateUtil.kt
@@ -1,12 +1,18 @@
 package com.dave.fish.util
 
 import org.joda.time.DateTime
-import org.joda.time.DateTimeZone
 
+import org.joda.time.DateTimeZone
 /**
  * Created by soul on 2017. 8. 23..
  */
-class DateUtil {
+object DateUtil {
+
+    val DATE_PATTERN_YEAR_MONTH_DAY = "yyyyMMdd"
+    val DATE_PATTERN_YEAR_MONTH_DAY_ADD_DASH = "yyyy-MM-dd"
+    val DATE_PATTERN_YEAR_MONTH = "yyyy-MM"
+    val DATE_PATTERN_ALL = "yyyy-MM-dd hh:mm:ss"
+    val DATE_TIME_ZONE_SEOUL = "Asia/Seoul"
 
     fun getCurrentDate(pattern:String) : String{
         val seoul = DateTimeZone.forID(DATE_TIME_ZONE_SEOUL)
@@ -18,20 +24,31 @@ class DateUtil {
         return DateTime()
     }
 
+    fun equalsDateWithCurrentDate(compareDate: DateTime) : Boolean{
+        val seoul = DateTimeZone.forID("Asia/Seoul")
+        val currentDate = DateTime(seoul)
+        return currentDate.toString(DATE_PATTERN_YEAR_MONTH_DAY_ADD_DASH) == compareDate.toString(DATE_PATTERN_YEAR_MONTH_DAY_ADD_DASH)
+    }
+
+    /**
+     * @method - 현재 년,월 이면서 날짜가 오늘보다 큰 경우 구분 짓는다.
+     * @why - 위의 조건에 해당하는 날짜의 데이터는 유동적으로 변경되는 값이기 때문에 ex)날씨 매번 데이터 갱신이 필요하다.
+     * @param compareDate - 비교할 날짜
+     */
+    fun isBiggerThanCurrentDate(compareDate: DateTime) : Boolean{
+        val seoul = DateTimeZone.forID("Asia/Seoul")
+        val currentDate = DateTime(seoul)
+        if(currentDate.toString(DATE_PATTERN_YEAR_MONTH) == compareDate.toString(DATE_PATTERN_YEAR_MONTH)){
+            if(currentDate.dayOfMonth <= compareDate.dayOfMonth){
+                return true
+            }
+        }
+
+        return false
+    }
+
     fun getDataWithPattern(dateTime: DateTime, pattern: String) : String{
         val seoul = DateTimeZone.forID(DATE_TIME_ZONE_SEOUL)
         return dateTime.withZone(seoul).toString(pattern)
-    }
-
-    private object Holder{
-        val INSTANCE = DateUtil()
-    }
-
-    companion object {
-        val INSTANCE: DateUtil by lazy{ Holder.INSTANCE }
-        val DATE_PATTERN_YEAR_MONTH_DAY = "yyyyMMdd"
-        val DATE_PATTERN_YEAR_MONTH_DAY_ADD_DASH = "yyyy-MM-dd"
-        val DATE_PATTERN_ALL = "yyyy-MM-dd hh:mm:ss"
-        val DATE_TIME_ZONE_SEOUL = "Asia/Seoul"
     }
 }
