3bd85abaa761d9e890efc17f55d2ef58023571d8
soulduse <soulduse@naver.com>
2017-10-14 21:26:21 +0900

    전역에서 context가져와서 사용할수 있게 추가

--- app/src/main/java/com/dave/fish/MyApplication.kt
+++ app/src/main/java/com/dave/fish/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.dave.fish
 
 import android.app.Application
+import android.content.Context
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
@@ -10,10 +11,9 @@ import io.realm.RealmConfiguration
  * Created by soul on 2017. 9. 14..
  */
 class MyApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
-        Realm.init(this)
-
         Realm.init(this)
         val config = RealmConfiguration.Builder()
                 .name(Realm.DEFAULT_REALM_NAME)
@@ -21,5 +21,12 @@ class MyApplication : Application() {
                 .deleteRealmIfMigrationNeeded()
                 .build()
         Realm.setDefaultConfiguration(config)
+
+        context = this
+    }
+
+    companion object {
+        var context: Context? = null
+            private set
     }
 }
