3f1d2e8f329e249c8d207609f92cf900b6a9bfff
soulduse <soulduse@naver.com>
2017-10-17 21:36:58 +0900

    데이터 클래스 기본 값 추

--- app/src/main/java/com/dave/fish/model/retrofit/ForecastSpaceData.kt
+++ app/src/main/java/com/dave/fish/model/retrofit/ForecastSpaceData.kt
@@ -1,6 +1,7 @@
 package com.dave.fish.model.retrofit
 
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by soul on 2017. 10. 13..
@@ -8,37 +9,37 @@ import java.util.*
  */
 class ForecastSpaceData {
 
-    var response : Response ?= null
+    var response: Response = Response()
 
     data class Response(
-            val header: Header,
-            val body: Body
+            val header: Header = Header(),
+            val body: Body = Body()
     )
 
     data class Header(
-            val resultCode: String,
-            val resultMsg: String
+            val resultCode: String = "",
+            val resultMsg: String = ""
     )
 
     data class Body(
-            val items: Items,
-            val numOfRows: Int,
-            val pageNo: Int,
-            val totalCount: Int
+            val items: Items = Items(),
+            val numOfRows: Int = 0,
+            val pageNo: Int = 0,
+            val totalCount: Int = 0
     )
 
     data class Items(
-            val item : List<Item>
+            val item: List<Item> = ArrayList()
     )
 
     data class Item(
-            val baseDate: Date,
-            val baseTime: String,
-            val category: String,
-            val fcstDate: Date,
-            val fcstTime: String,
-            val fcstValue: Double,
-            val nx: Int,
-            val ny: Int
+            val baseDate: Date = Date(),
+            val baseTime: String = "",
+            val category: String = "",
+            val fcstDate: Date = Date(),
+            val fcstTime: String = "",
+            val fcstValue: Double = 0.0,
+            val nx: Int = 0,
+            val ny: Int = 0
     )
 }
