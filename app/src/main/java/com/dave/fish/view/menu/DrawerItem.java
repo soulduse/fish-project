55d3a56049f0f2b17954f8be472274619ed744fc
soulduse <soulduse@naver.com>
2017-10-23 01:07:51 +0900

    DrawerMenu를 위한 코드 추

---
+++ app/src/main/java/com/dave/fish/view/menu/DrawerItem.java
@@ -0,0 +1,26 @@
+package com.dave.fish.view.menu;
+
+import android.view.ViewGroup;
+
+public abstract class DrawerItem<T extends DrawerAdapter.ViewHolder> {
+
+    public boolean isChecked;
+
+    public abstract T createViewHolder(ViewGroup parent);
+
+    public abstract void bindViewHolder(T holder);
+
+    public DrawerItem setChecked(boolean isChecked) {
+        this.isChecked = isChecked;
+        return this;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public boolean isSelectable() {
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/dave/fish/view/menu/MenuDrawer.kt
@@ -0,0 +1,8 @@
+package com.dave.fish.view.menu
+
+/**
+ * Created by soul on 2017. 10. 22..
+ */
+enum class MenuDrawer(val position : Int) {
+    INFO(0), MAP(1), CATCH(3), CHAT(4), ALARM(5), MAIL(7), SHARE(8);
+}
