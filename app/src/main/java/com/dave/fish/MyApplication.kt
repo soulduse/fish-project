3a4f2adbaf3941e0fa4c1925d453a28b8af83c2e
soulduse <soulduse@naver.com>
2017-11-10 23:53:41 +0900

    커스텀 로그 생

--- app/src/main/java/com/dave/fish/MyApplication.kt
+++ app/src/main/java/com/dave/fish/MyApplication.kt
@@ -4,6 +4,10 @@ import android.app.Application
 import android.content.Context
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+
+
 
 
 
@@ -14,6 +18,9 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        DEBUG = isDebuggable(this)
+
         Realm.init(this)
         val config = RealmConfiguration.Builder()
                 .name(Realm.DEFAULT_REALM_NAME)
@@ -25,8 +32,30 @@ class MyApplication : Application() {
         context = this
     }
 
+    /**
+     * get Debug Mode
+     *
+     * @param context
+     * @return
+     */
+    private fun isDebuggable(context: Context): Boolean {
+        var debuggable = false
+
+        val pm = context.packageManager
+        try {
+            val appinfo = pm.getApplicationInfo(context.packageName, 0)
+            debuggable = 0 != appinfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+        } catch (e: PackageManager.NameNotFoundException) {
+            /* debuggable variable will remain false */
+        }
+
+        return debuggable
+    }
+
     companion object {
         var context: Context? = null
             private set
+
+        var DEBUG = false
     }
 }
---
+++ app/src/main/java/com/dave/fish/util/DLog.kt
@@ -0,0 +1,44 @@
+package com.dave.fish.util
+
+import android.util.Log
+import com.dave.fish.MyApplication
+
+/**
+ * Created by soul on 2017. 11. 10..
+ */
+object DLog {
+
+    private val TAG : String = "Dave"
+
+    fun e(message : String){
+        if(MyApplication.DEBUG) Log.e(TAG, buildLogMsg(message))
+    }
+
+    fun w(message : String){
+        if(MyApplication.DEBUG) Log.w(TAG, buildLogMsg(message))
+    }
+
+    fun i(message : String){
+        if(MyApplication.DEBUG) Log.i(TAG, buildLogMsg(message))
+    }
+
+    fun d(message : String){
+        if(MyApplication.DEBUG) Log.d(TAG, buildLogMsg(message))
+    }
+
+    fun v(message : String){
+        if(MyApplication.DEBUG) Log.v(TAG, buildLogMsg(message))
+    }
+
+    private fun buildLogMsg(message : String) : String{
+        val ste = Thread.currentThread().stackTrace[4]
+        val sb = StringBuilder()
+        sb.append("[")
+        sb.append(ste.fileName.replace(".java", ""))
+        sb.append("::")
+        sb.append(ste.methodName)
+        sb.append("]")
+        sb.append(message)
+        return sb.toString()
+    }
+}
