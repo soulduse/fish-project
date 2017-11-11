481f14ed220031506ba2ddc573f9d5afff2daa81
soulduse <soulduse@naver.com>
2017-11-11 13:13:26 +0900

    Update DateUtil

--- app/src/main/java/com/dave/fish/util/DateUtil.kt
+++ app/src/main/java/com/dave/fish/util/DateUtil.kt
@@ -42,6 +42,18 @@ object DateUtil {
         return currentDate.toString(DATE_PATTERN_YEAR_MONTH) == compareDate.toString(DATE_PATTERN_YEAR_MONTH)
     }
 
+    fun isBeforeToday(compareDate: DateTime) : Boolean{
+        val seoul = DateTimeZone.forID(DATE_TIME_ZONE_SEOUL)
+        val currentDate = DateTime(seoul)
+        return compareDate.isBefore(currentDate)
+    }
+
+    fun isThisMonth(date : DateTime) : Boolean{
+        val seoul = DateTimeZone.forID(DATE_TIME_ZONE_SEOUL)
+        val currentDate = DateTime(seoul)
+        return currentDate.toString(DATE_PATTERN_YEAR_MONTH) == date.toString(DATE_PATTERN_YEAR_MONTH)
+    }
+
     fun getDataWithPattern(dateTime: DateTime, pattern: String) : String{
         val seoul = DateTimeZone.forID(DATE_TIME_ZONE_SEOUL)
         return dateTime.withZone(seoul).toString(pattern)
