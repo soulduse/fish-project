5a222880b18bf8aaaf85c65c0707be046b6e421a
soulduse <soulduse@naver.com>
2017-11-14 23:07:43 +0900

    커스텀 로그 예외처

--- app/src/main/java/com/dave/fish/util/DLog.kt
+++ app/src/main/java/com/dave/fish/util/DLog.kt
@@ -31,12 +31,12 @@ object DLog {
     }
 
     private fun buildLogMsg(message : String) : String{
-        val ste = Thread.currentThread().stackTrace[4]
+        val ste : StackTraceElement ?= Thread.currentThread().stackTrace[4]
         val sb = StringBuilder()
         sb.append("[")
-        sb.append(ste.fileName.replace(".java", ""))
+        sb.append(ste?.fileName?.replace(".java", ""))
         sb.append("::")
-        sb.append(ste.methodName)
+        sb.append(ste?.methodName)
         sb.append("]")
         sb.append(message)
         return sb.toString()
