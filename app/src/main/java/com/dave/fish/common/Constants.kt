284e646490dca56d50ac7f329681c1dde718d886
soulduse <soulduse@naver.com>
2017-11-17 01:06:34 +0900

    상수 선

---
+++ app/src/main/java/com/dave/fish/common/Constants.kt
@@ -0,0 +1,21 @@
+package com.dave.fish.common
+
+/**
+ * Created by soul on 2017. 11. 16..
+ */
+object Constants {
+
+    // Service
+    val EXTRA_RECEIVER = "EXTRA_RECEIVER"
+    val EXTRA_RECEIVER_ID = 1001
+
+    // Notification
+    val NOTIFICATION_CHANEL = "notification_chanel"
+    val EXTRA_NOTIFICATION_ID = 1001
+    val EXTRA_NOTIFIER = "EXTRA_NOTIFIER"
+
+    // Location
+    val UPDATE_COMPLETE_LOCATION = 200
+    val LOCATION_SERVICE_RESULT = "com.dave.fish.LOCATION_RESULT"
+    val LOCATION_SERVICE_MESSAGE = "com.dave.fish.LOCATION_MESSAGE"
+}
