67ba5b5ce6fbe83042aafb54920065ad5bd728bd
soulduse <soulduse@naver.com>
2017-12-06 23:03:19 +0900

    커스텀 웹뷰

---
+++ app/src/main/java/com/dave/fish/view/TouchWebView.kt
@@ -0,0 +1,23 @@
+package com.dave.fish.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.webkit.WebView
+
+/**
+ * Created by soul on 2017. 12. 5..
+ */
+class TouchWebView : WebView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) : super(context, attributeSet, defStyleAttr)
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        requestDisallowInterceptTouchEvent(true)
+        return super.onTouchEvent(event)
+    }
+}
