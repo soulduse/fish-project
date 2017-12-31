8f3e6dea5be512dfedd8210acf574aaced9906ac
soulduse <soulduse@naver.com>
2017-12-31 22:24:37 +0900

    Api Provider 생성 기존 내용 변경하여 재구성함

---
+++ app/src/main/java/com/dave/fish/api/ApiProvider.kt
@@ -0,0 +1,46 @@
+package com.dave.fish.api
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+/**
+ * Created by soul on 2017. 12. 31..
+ */
+object ApiProvider {
+
+    private const val TIDE_URL = "http://www.khoa.go.kr/swtc/"
+    private const val KMA_URL = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/"
+
+    fun provideTideApi(): TideApi {
+        return Retrofit.Builder()
+                .baseUrl(TIDE_URL)
+                .client(provideOkHttpClient(provideLoggingInterceptor()))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build()
+                .create(TideApi::class.java)
+    }
+
+    fun provideKmaApi(): KmaApi {
+        return Retrofit.Builder()
+                .baseUrl(KMA_URL)
+                .client(provideOkHttpClient(provideLoggingInterceptor()))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build()
+                .create(KmaApi::class.java)
+    }
+
+    private fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        val b = OkHttpClient.Builder()
+        b.addInterceptor(interceptor)
+        return b.build()
+    }
+
+    private fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
+
+}
