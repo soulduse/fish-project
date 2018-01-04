19a7df8c9235cb2deefc30ff765c113069447373
soulduse <soulduse@naver.com>
2018-01-04 23:27:21 +0900

    alarmManager 부분 구현

--- app/src/main/java/com/dave/fish/ui/alarm/AlarmFragment.kt
+++ app/src/main/java/com/dave/fish/ui/alarm/AlarmFragment.kt
@@ -1,24 +1,38 @@
 package com.dave.fish.ui.alarm
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.NumberPicker
 import com.dave.fish.R
 import com.dave.fish.ui.BaseFragment
 import kotlinx.android.synthetic.main.fragment_alaram.*
+import org.joda.time.DateTime
 
 /**
  * Created by soul on 2017. 12. 16..
  */
 class AlarmFragment : BaseFragment() {
 
+    lateinit var alarmMgr: AlarmManager
+
+    lateinit var alarmIntent: Intent
+
+    lateinit var pendingIntent: PendingIntent
+
     override fun getContentId(): Int = R.layout.fragment_alaram
 
     override fun initViews(savedInstanceState: Bundle?) {
         initNumberPicker()
+        initAlarm()
+        getAlarmTime()
     }
 
     override fun initData() {
-        initAlarm()
+        initAlarmData()
     }
 
     private fun initNumberPicker() {
@@ -37,7 +51,37 @@ class AlarmFragment : BaseFragment() {
         num_picker_min.setOnValueChangedListener(changedListener)
     }
 
-    private fun initAlarm(){
+    private fun initAlarm() {
+        alarmMgr = mContext?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmIntent = Intent("com.dave.fish.START_ALARM")
+        pendingIntent = PendingIntent.getBroadcast(mContext, 0, alarmIntent, 0)
+
+        start_alarm.setOnClickListener {
+            setExactAlarm(
+                    AlarmManager.RTC_WAKEUP,
+                    getAlarmTime(),
+                    pendingIntent
+            )
+        }
+    }
+
+    fun setExactAlarm(type: Int, triggerAtMillis: Long, operation: PendingIntent ) {
+        if (Build.VERSION.SDK_INT >= 23)
+            alarmMgr.setExactAndAllowWhileIdle(type, triggerAtMillis, operation)
+        else if (Build.VERSION.SDK_INT >= 19)
+            alarmMgr.setExact(type, triggerAtMillis, operation)
+        else
+            alarmMgr.set(type, triggerAtMillis, operation)
+    }
+
+    private fun getAlarmTime(): Long{
+        return DateTime().apply {
+            plusHours(num_picker_hour.value)
+            plusMinutes(num_picker_min.value * STEP_NUMBER)
+        }.millis
+    }
+
+    private fun initAlarmData(){
         result_time.text = resources.getString(
                 R.string.number_picker_selected_values,
                 num_picker_hour.value,
@@ -46,7 +90,7 @@ class AlarmFragment : BaseFragment() {
     }
 
     private val changedListener = NumberPicker.OnValueChangeListener { _, _, _ ->
-        initAlarm()
+        initAlarmData()
     }
 
     companion object {
