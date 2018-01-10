3d2039ad825636eccdf310d86ee0c39a8b996354
soulduse <soulduse@naver.com>
2018-01-10 23:37:35 +0900

    웹뷰에서 Back키 누를시 웹뷰 스택이 있다면 뒤로가기

--- app/src/main/java/com/dave/fish/ui/web/WebFragment.kt
+++ app/src/main/java/com/dave/fish/ui/web/WebFragment.kt
@@ -2,15 +2,14 @@ package com.dave.fish.ui.web
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.support.v4.app.Fragment
-import android.view.*
-import android.webkit.WebView
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.webkit.WebViewClient
 import com.dave.fish.R
 import com.dave.fish.ui.main.MainActivity
-import com.dave.fish.util.DLog
 import kotlinx.android.synthetic.main.fragment_web.*
 
 /**
@@ -21,60 +20,41 @@ class WebFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.fragment_web, container, false)
 
-    private val goBackHandler = @SuppressLint("HandlerLeak")
-    object : Handler(){
-        override fun handleMessage(msg: Message?) {
-            when(msg?.what){
-                1->{
-                    webViewGoback()
-                }
-            }
-        }
-    }
-
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        webview_weather.apply {
-            webViewClient = CustomWebViewClient()
+        with(webview_weather){
             settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
             isFocusable = true
             isFocusableInTouchMode = true
             loadUrl(arguments?.getString("url"))
-        }
 
-        webview_weather.setOnKeyListener(View.OnKeyListener { _, keyCode, keyEvent ->
-            if(keyEvent.action != KeyEvent.ACTION_DOWN)
-                return@OnKeyListener true
+            setOnKeyListener(View.OnKeyListener { _, keyCode, keyEvent ->
+                when{
+                    keyEvent.action != KeyEvent.ACTION_DOWN -> return@OnKeyListener true
 
-            if(keyCode == KeyEvent.KEYCODE_BACK){
-                if(webview_weather.canGoBack()){
-                    DLog.w("canGoBack")
-                    webview_weather.goBack()
-                }else{
-                    DLog.w("canNotGoBack")
-                    (activity as MainActivity).onBackPressed()
+                    keyCode == KeyEvent.KEYCODE_BACK ->{
+                        backPressed()
+                        return@OnKeyListener true
+                    }
+
+                    else -> return@OnKeyListener false
                 }
-                return@OnKeyListener true
-            }
-            return@OnKeyListener false
-        })
+            })
+        }
 
         fab_reload.setOnClickListener {
             webview_weather.reload()
         }
     }
 
-    private fun webViewGoback() {
-        DLog.w("webViewGoback")
-        webview_weather.goBack()
-    }
-
-    inner class CustomWebViewClient : WebViewClient(){
-        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-            view?.loadUrl(url)
-            return true
+    private fun backPressed() {
+        if (webview_weather.canGoBack()) {
+            webview_weather.goBack()
+        } else {
+            (activity as MainActivity).onBackPressed()
         }
     }
 
