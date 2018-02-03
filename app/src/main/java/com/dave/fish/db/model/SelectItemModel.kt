4862d9432faaca2a30db9d5a3fcabca3bdf61766
soulduse <soulduse@naver.com>
2018-02-03 16:47:16 +0900

    Realm 스피너 관련 모델값 수정

--- app/src/main/java/com/dave/fish/db/model/SelectItemModel.kt
+++ app/src/main/java/com/dave/fish/db/model/SelectItemModel.kt
@@ -1,14 +1,17 @@
 package com.dave.fish.db.model
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
 /**
  * Created by soul on 2017. 9. 16..
+ *
+ * keyTide  : 0 - main
+ *          : 1 - today
  */
 open class SelectItemModel : RealmObject(){
-    var doNm: String = ""
-    var firstPosition = 0
+    @PrimaryKey
+    var keyTide: Int = 0
     var postName: String = ""
-    var secondPosition = 0
-    var isTodayTide = false
+    var doNm: String = ""
 }
--- app/src/main/java/com/dave/fish/db/model/SpinnerFirstModel.kt
+++ app/src/main/java/com/dave/fish/db/model/SpinnerFirstModel.kt
@@ -2,12 +2,15 @@ package com.dave.fish.db.model
 
 import io.realm.RealmList
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
 /**
  * Created by soul on 2017. 9. 16..
  */
 
 open class SpinnerFirstModel : RealmObject(){
+    @PrimaryKey
+    var id: String = ""
     var areaName :String = ""
     var secondSpinnerItems : RealmList<SpinnerSecondModel> = RealmList()
 }
