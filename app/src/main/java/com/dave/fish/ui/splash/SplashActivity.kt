46c04bd23dbfefed14c66b87ac1858c1e4a68bcc
soulduse <soulduse@naver.com>
2018-02-03 16:51:45 +0900

    스플래시 화면에서 가공된 데이터 Realm에 update하도록 수정

--- app/src/main/java/com/dave/fish/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/dave/fish/ui/splash/SplashActivity.kt
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import com.dave.fish.api.ApiProvider
 import com.dave.fish.api.Network
 import com.dave.fish.api.NetworkCallback
-import com.dave.fish.db.RealmController
+import com.dave.fish.db.RealmProvider
 import com.dave.fish.db.RealmListener
 import com.dave.fish.api.model.GisModel
 import com.dave.fish.ui.main.MainActivity
@@ -17,7 +17,7 @@ import com.dave.fish.util.DLog
  */
 class SplashActivity : AppCompatActivity(){
 
-    private lateinit var mRealmController : RealmController
+    private lateinit var mRealmController : RealmProvider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,36 +26,35 @@ class SplashActivity : AppCompatActivity(){
     }
 
     private fun initRealm() {
-        mRealmController = RealmController.instance
+        mRealmController = RealmProvider.instance
         mRealmController.setListener(realmListener)
     }
 
     private fun initSpinnerData(){
         if(isEmptyRealmSpinner()){
-            initSelectSpinner()
             initDataSpinner()
+            initSelectSpinner()
             return
         }
 
         moveMain()
     }
 
-    private fun isEmptyRealmSpinner(): Boolean = mRealmController.getSpinnerItems().isEmpty()
+    private fun isEmptyRealmSpinner(): Boolean = mRealmController.getSpinner().isEmpty()
 
     private fun initDataSpinner() {
         Network.request(ApiProvider.provideTideApi().getGisData(),
                 NetworkCallback<GisModel>().apply {
                     success = { gisModel ->
-                        val dataList = gisModel.data
-                        val gisMap = dataList.let {
-                            dataList.groupBy {
-                                it.doNm
-                            }
-                        }.filter {
-                            it.key.isNotEmpty()
-                        }
+                        val gisMap = gisModel.data
+                                .filter { !it.doNm.isNullOrEmpty() }
+                                .filter { it.doNm != "황해남도" }
+                                .filter { it.useYn == "Y" }
+                                .sortedBy { it.doNm }
+                                .groupBy { it.doNm }
 
-                        mRealmController.setSpinner(gisMap)
+                        DLog.w(gisMap.toString())
+                        mRealmController.setInitSpinner(gisMap)
                     }
 
                     error = {
@@ -66,11 +65,9 @@ class SplashActivity : AppCompatActivity(){
 
     private fun initSelectSpinner(){
         mRealmController
-                .initSelectedSpinnerItem(
+                .setSelectSpinner(
                         FIRST_SPINNER_AREA,
-                        SECOND_SPINNER_AREA,
-                        FIRST_SPINNER_POSITION,
-                        SECOND_SPINNER_POSITION
+                        SECOND_SPINNER_AREA
                 )
     }
 
@@ -92,7 +89,5 @@ class SplashActivity : AppCompatActivity(){
     companion object {
         private val FIRST_SPINNER_AREA = "충청남도"
         private val SECOND_SPINNER_AREA = "안흥"
-        private val FIRST_SPINNER_POSITION = 10
-        private val SECOND_SPINNER_POSITION = 3
     }
 }
