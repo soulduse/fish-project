54eda2b19db7d868e88df6bfd84a840eb80db197
soulduse <soulduse@naver.com>
2018-02-03 16:45:09 +0900

    RealmProvider 리팩토링

--- app/src/main/java/com/dave/fish/db/RealmProvider.kt
+++ app/src/main/java/com/dave/fish/db/RealmProvider.kt
@@ -1,7 +1,7 @@
 package com.dave.fish.db
 
 import com.dave.fish.api.model.GisModel
-import com.dave.fish.api.model.WeeklyModel
+import com.dave.fish.common.Constants
 import com.dave.fish.db.model.SelectItemModel
 import com.dave.fish.db.model.SpinnerFirstModel
 import com.dave.fish.db.model.SpinnerSecondModel
@@ -66,47 +66,27 @@ class RealmProvider {
         }
     }
 
-    fun setSelectSpinner(key: String,
+    fun setSelectSpinner(areaName: String,
                          postName: String,
-                         isTodayTide: Boolean = false,
+                         keyTide: Int = Constants.KEY_TIDE_MAIN_SPINNER,
                          listener: ()->Unit = {}){
 
         val selectItemModel = SelectItemModel().apply {
-            this.key = 0
-            this.doNm = key
+            this.keyTide = keyTide
+            this.doNm = areaName
             this.postName = postName
-            this.isTodayTide = isTodayTide
         }
 
         getRealm().use {
             it.executeTransactionAsync({
                 it.copyToRealmOrUpdate(selectItemModel)
-                DLog.w("setSelectSpinner set")
             },{
                 // 트랜잭션이 성공하였습니다.
                 realmListener?.onTransactionSuccess(listener)
-                DLog.w("setSelectSpinner success")
             },{
                 DLog.w("setSelectSpinner fail cause --> ${it.message}")
             })
         }
-    }
-
-    fun findSelectedSecondModel(isTodayTide: Boolean = false): SpinnerSecondModel {
-        val realm = getRealm()
-        val selectedItem = realm.where(SelectItemModel::class.java)
-                .equalTo("isTodayTide", isTodayTide)
-                .findFirst()
-
-        DLog.w("findSelectedSecondModel doNm : ${selectedItem.doNm}, postName: ${selectedItem.postName}")
-        return findByPostName(selectedItem.doNm, selectedItem.postName)
-    }
-
-    fun getSelectedItem(isTodayTide: Boolean = false): SelectItemModel? {
-        val realm = getRealm()
-        return realm.where(SelectItemModel::class.java)
-                .equalTo("isTodayTide", isTodayTide)
-                .findFirst()
     }
 
     fun getSpinner(): List<SpinnerFirstModel>{
@@ -115,110 +95,25 @@ class RealmProvider {
                 .findAll().toList()
     }
 
-    private fun findByPostName(doNm: String, postName: String): SpinnerSecondModel {
+    fun getSelectedItem(keyTide: Int = Constants.KEY_TIDE_MAIN_SPINNER): SelectItemModel{
         val realm = getRealm()
-        return realm.where(SpinnerFirstModel::class.java)
-                .equalTo("areaName", doNm)
-                .findFirst()
-                .secondSpinnerItems.first { second -> second.obsPostName == postName }
+        return realm.where(SelectItemModel::class.java)
+                .equalTo("keyTide", keyTide)
+                .findAll()
+                .toList()
+                .first()
     }
 
-    private fun createTideWeekly(weeklyData: WeeklyModel.WeeklyData, postId: String){
-        getRealm().use {
-            it.executeTransactionAsync { db ->
-                db.createObject(TideWeeklyModel::class.java, weeklyData.obsPostName + "_" + weeklyData.searchDate).apply {
-                    this.postId = postId
-                    this.am = weeklyData.am
-                    this.dateMoon = weeklyData.dateMoon
-                    this.dateSun = weeklyData.dateSun
-                    this.flgView = weeklyData.flgView
-                    this.lvl1 = weeklyData.lvl1
-                    this.lvl2 = weeklyData.lvl2
-                    this.lvl3 = weeklyData.lvl3
-                    this.lvl4 = weeklyData.lvl4
-                    this.mool7 = weeklyData.mool7
-                    this.mool8 = weeklyData.mool8
-                    this.moolNormal = weeklyData.moolNormal
-                    this.obsPostName = weeklyData.obsPostName
-                    this.obsLat = weeklyData.obsLat
-                    this.obsLon = weeklyData.obsLon
-                    this.searchDate = DateTime(weeklyData.searchDate).toDate()
-                    this.temp = weeklyData.temp
-                    this.weatherChar = weeklyData.weatherChar
-                }
-            }
-        }
-    }
+    fun getSecondSpinnerItem(keyTide: Int = Constants.KEY_TIDE_MAIN_SPINNER): SpinnerSecondModel? {
+        val selectedItem = getSelectedItem(keyTide)
 
-    private fun insertTideWeekly(weeklyData: WeeklyModel.WeeklyData, postId: String){
-        getRealm().use {
-            it.executeTransactionAsync { db ->
-                val tideWeeklyModel = TideWeeklyModel()
-                with(tideWeeklyModel){
-                    key = weeklyData.obsPostName + "_" + weeklyData.searchDate
-                    this.postId = postId
-                    dateMoon = weeklyData.dateMoon
-                    dateSun = weeklyData.dateSun
-                    flgView = weeklyData.flgView
-                    lvl1 = weeklyData.lvl1
-                    lvl2 = weeklyData.lvl2
-                    lvl3 = weeklyData.lvl3
-                    lvl4 = weeklyData.lvl4
-                    mool7 = weeklyData.mool7
-                    mool8 = weeklyData.mool8
-                    moolNormal = weeklyData.moolNormal
-                    obsPostName = weeklyData.obsPostName
-                    obsLat = weeklyData.obsLat
-                    obsLon = weeklyData.obsLon
-                    searchDate = DateTime(weeklyData.searchDate).toDate()
-                    temp = weeklyData.temp
-                    this?.am = weeklyData?.am
-                    this?.weatherChar = weeklyData?.weatherChar
-                }
-                db.insertOrUpdate(tideWeeklyModel)
-            }
-        }
-    }
-
-    private fun updateTideWeekly(weeklyData: WeeklyModel.WeeklyData, postId: String){
-        getRealm().use {
-            val tideWeeklyModel = findTideWeekly(weeklyData.obsPostName + "_" + weeklyData.searchDate)
-            it.executeTransaction { db ->
-                with(tideWeeklyModel){
-                    this.postId = postId
-                    dateMoon = weeklyData.dateMoon
-                    dateSun = weeklyData.dateSun
-                    flgView = weeklyData.flgView
-                    lvl1 = weeklyData.lvl1
-                    lvl2 = weeklyData.lvl2
-                    lvl3 = weeklyData.lvl3
-                    lvl4 = weeklyData.lvl4
-                    mool7 = weeklyData.mool7
-                    mool8 = weeklyData.mool8
-                    moolNormal = weeklyData.moolNormal
-                    obsPostName = weeklyData.obsPostName
-                    obsLat = weeklyData.obsLat
-                    obsLon = weeklyData.obsLon
-                    searchDate = DateTime(weeklyData.searchDate).toDate()
-                    temp = weeklyData.temp
-                    this?.am = weeklyData?.am
-                    this?.weatherChar = weeklyData?.weatherChar
-                }
-            }
-        }
-    }
-
-    fun setTideWeekly(weeklyData: WeeklyModel.WeeklyData, postId: String) {
-        getRealm().use {
-            if (findSizeOfTideWeekly() > 0) {
-                try{
-                    updateTideWeekly(weeklyData, postId)
-                }catch (e: Exception){
-                    insertTideWeekly(weeklyData, postId)
-                }
-            } else {
-                createTideWeekly(weeklyData, postId)
-            }
+        selectedItem.let { selected->
+            return getSpinner()
+                    .filter { it.areaName ==  selected.doNm }
+                    .map { it.secondSpinnerItems }
+                    .first()
+                    .toList()
+                    .find { it.obsPostName ==  selected.postName }
         }
     }
 
@@ -233,14 +128,6 @@ class RealmProvider {
                         , to)
                 .equalTo("postId", postId)
                 .findAll()
-    }
-
-    fun findTideWeelyList() : RealmResults<TideWeeklyModel>{
-        getRealm().use {
-            return it.where(TideWeeklyModel::class.java)
-                    .findAll()
-                    .sort("key")
-        }
     }
 
     fun findTideWeekly(key: String) : TideWeeklyModel {
@@ -259,14 +146,17 @@ class RealmProvider {
         this.realmListener = realmListener
     }
 
+    fun <T> writeData(item: T){
+        getRealm().use({
+            it.beginTransaction()
+            it.copyToRealmOrUpdate(item as RealmObject)
+            it.commitTransaction()
+        })
+    }
+
     fun <T : RealmObject> copyData(e: T): RealmModel {
         val realm = getRealm()
         return realm.copyFromRealm(e)
-    }
-
-    fun <T : RealmObject> copyDataOrUpdate(e: T): RealmModel {
-        val realm = getRealm()
-        return realm.copyToRealmOrUpdate(e)
     }
 
     fun closeRealm() {
