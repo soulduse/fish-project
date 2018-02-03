474a45568f67a63d28436fd17b17efeac449fbf4
soulduse <soulduse@naver.com>
2018-02-03 16:53:11 +0900

    스피너 정상작동 하지 않아 커스텀스피너 코드 리펙토링

--- app/src/main/java/com/dave/fish/common/PickTideDialog.kt
+++ app/src/main/java/com/dave/fish/common/PickTideDialog.kt
@@ -26,14 +26,14 @@ class PickTideDialog : DialogFragment() {
         val inflater = activity?.layoutInflater
         val rootView = inflater?.inflate(R.layout.dialog_pick_tide, null)
 
-        val customAreasSpinner = rootView?.findViewById<CustomAreasSpinner>(R.id.custom_areas_spinner).apply {
-            this?.init(true)
+        val sideSpinner = rootView?.findViewById<CustomAreasSpinner>(R.id.custom_areas_spinner).apply {
+            this?.init(Constants.KEY_TIDE_SIDE_SPINNER)
         }
 
         builder.setView(rootView)
                 // Add action buttons
                 .setPositiveButton("설정", DialogInterface.OnClickListener { _, _ ->
-                    customAreasSpinner?.getPickedValueOfTide(mListener)
+                    sideSpinner?.getPickedValueOfTide(mListener)
                 })
                 .setNegativeButton("취소", DialogInterface.OnClickListener { _, _ ->
                     this.dismiss()
--- app/src/main/java/com/dave/fish/ui/CustomAreasSpinner.kt
+++ app/src/main/java/com/dave/fish/ui/CustomAreasSpinner.kt
@@ -5,34 +5,32 @@ import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import com.dave.fish.R
 import com.dave.fish.api.ApiProvider
 import com.dave.fish.api.Network
 import com.dave.fish.api.NetworkCallback
 import com.dave.fish.api.model.SidePanelData
 import com.dave.fish.api.model.SidePanelModel
-import com.dave.fish.db.RealmController
+import com.dave.fish.common.Constants
 import com.dave.fish.db.RealmListener
+import com.dave.fish.db.RealmProvider
 import com.dave.fish.db.model.SelectItemModel
+import com.dave.fish.db.model.SpinnerSecondModel
 import com.dave.fish.util.DLog
-import io.realm.Realm
+import kotlinx.android.synthetic.main.areas_spinner.view.*
 
 /**
  * Created by soul on 2017. 12. 17..
  */
 class CustomAreasSpinner : ConstraintLayout {
 
-    private lateinit var bg : LinearLayout
-    private lateinit var spinnerLoc : Spinner
-    private lateinit var spinnerMap : Spinner
-
-    private var selectedSpinner : SelectItemModel = SelectItemModel()
-    private var firstSpinnerPosition = 0
-    private lateinit var mRealmController : RealmController
-    private lateinit var realm : Realm
-    private var isTodayTide: Boolean = false
+    private var keyTide: Int = Constants.KEY_TIDE_MAIN_SPINNER
     private var listenerChangedSpinner : ()-> Unit = {}
+    private var secondPosition = 0
 
     constructor(context : Context) : super(context){
         initRealm()
@@ -40,12 +38,13 @@ class CustomAreasSpinner : ConstraintLayout {
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs){
         initRealm()
-        getAttrs(attrs)
     }
 
-    constructor(context: Context, attrs: AttributeSet, difStyle: Int) : super(context, attrs){
-        initRealm()
-        getAttrs(attrs, difStyle)
+
+    fun init(keyTide: Int) : CustomAreasSpinner {
+        this.keyTide = keyTide
+        initView()
+        return this
     }
 
     private fun initView(){
@@ -53,36 +52,38 @@ class CustomAreasSpinner : ConstraintLayout {
         val li = context.getSystemService(infService) as LayoutInflater
         val v = li.inflate(R.layout.areas_spinner, this, false)
         addView(v)
-
-        bg = findViewById<ConstraintLayout>(R.id.bg) as LinearLayout
-        spinnerLoc = findViewById<Spinner>(R.id.spinner_loc) as Spinner
-        spinnerMap = findViewById<Spinner>(R.id.spinner_map) as Spinner
-
         initSpinner()
     }
 
-    private fun getAttrs(attrs: AttributeSet){
-
-    }
-
-    private fun getAttrs(attrs: AttributeSet, defStyle: Int){
-
-    }
 
     private fun initRealm() {
-        realm = Realm.getDefaultInstance()
-        mRealmController = RealmController.instance
-        mRealmController.setListener(realmListener)
+        RealmProvider.instance.setListener(realmListener)
     }
 
     private fun initSpinner() {
-        spinnerLoc.onItemSelectedListener = spinnerListener
-        spinnerMap.onItemSelectedListener = spinnerListener
+        spinner_loc.onItemSelectedListener = spinnerListener
+        spinner_map.onItemSelectedListener = spinnerListener
 
-        setSpinnerAdapter(spinnerLoc, mRealmController.getSpinnerItems())
-        selectedSpinner = mRealmController.findSelectedSpinnerItem(isTodayTide)
-        spinnerLoc.setSelection(selectedSpinner.firstPosition, false)
+        val firstNames = RealmProvider.instance.getSpinner().map { it.areaName }
+
+        setSpinnerAdapter(spinner_loc, firstNames)
+
+        val selectedSpinner: SelectItemModel ?= RealmProvider.instance.getSelectedItem(keyTide)
+
+        selectedSpinner?.let {
+            val firstPosition = getFirstSpinnerPosition(it)
+            spinner_loc.setSelection(firstPosition)
+
+            secondPosition = getSecondSpinnerPosition(it)
+        }
     }
+
+    private fun getFirstSpinnerPosition(selectedSpinner: SelectItemModel) =
+            RealmProvider.instance.getSpinner().indexOfFirst { it.areaName == selectedSpinner.doNm }
+
+    private fun getSecondSpinnerPosition(selectedSpinner: SelectItemModel) =
+            getSecondSpinner(selectedSpinner.doNm)
+                    .indexOfFirst { it.obsPostName == selectedSpinner.postName }
 
     private fun setSpinnerAdapter(spinner : Spinner, items : List<String>){
         val spinnerArrayAdapter = ArrayAdapter<String>(context, R.layout.spinner_item, items)
@@ -92,7 +93,7 @@ class CustomAreasSpinner : ConstraintLayout {
 
     private val realmListener = object : RealmListener {
         override fun onSpinnerSuccess() {
-            setSpinnerAdapter(spinnerLoc, RealmController.instance.getSpinnerItems())
+            DLog.w("success spinner!")
         }
 
         override fun onTransactionSuccess(listener: ()->Unit) {
@@ -101,10 +102,12 @@ class CustomAreasSpinner : ConstraintLayout {
         }
     }
 
-    fun init(isTodayTide: Boolean) : CustomAreasSpinner {
-        this.isTodayTide = isTodayTide
-        initView()
-        return this
+    private fun getSecondSpinner(areaName: String): List<SpinnerSecondModel>{
+        return RealmProvider.instance.getSpinner()
+                .filter { it.areaName == areaName }
+                .map { it.secondSpinnerItems }
+                .first()
+                .toList()
     }
 
     fun initListener(changed : ()-> Unit){
@@ -120,26 +123,17 @@ class CustomAreasSpinner : ConstraintLayout {
             val spinner = parent as Spinner
 
             when(spinner){
-                spinnerLoc ->{
-                    setSpinnerAdapter(spinnerMap, mRealmController.getSelectedSpinnerItem(spinnerLoc.selectedItem.toString())!!)
-                    firstSpinnerPosition = pos
-                    selectedSpinner.let {
-                        if(selectedSpinner.doNm == spinnerLoc.selectedItem.toString()){
-                            spinnerMap.setSelection(selectedSpinner.secondPosition)
-                        }else{
-                            spinnerMap.setSelection(0)
-                        }
-                    }
+                spinner_loc ->{
+                    setSpinnerAdapter(spinner_map, getSecondSpinner(spinner_loc.selectedItem.toString()).map { it.obsPostName })
+//                    spinner_map.setSelection(secondPosition)
                 }
 
-                spinnerMap->{
-                    mRealmController
-                            .setSelectedSpinnerItem(
-                                    spinnerLoc.selectedItem.toString(),
-                                    spinnerMap.selectedItem.toString(),
-                                    firstSpinnerPosition,
-                                    pos,
-                                    isTodayTide,
+                spinner_map->{
+                    RealmProvider.instance
+                            .setSelectSpinner(
+                                    spinner_loc.selectedItem.toString(),
+                                    spinner_map.selectedItem.toString(),
+                                    keyTide,
                                     listenerChangedSpinner
                             )
                 }
@@ -148,23 +142,23 @@ class CustomAreasSpinner : ConstraintLayout {
     }
 
     fun getPickedValueOfTide(event : (values: SidePanelData)->Unit){
-        val secondSpinnerItem = mRealmController.findSelectedSecondModel(isTodayTide)
-        val postId = secondSpinnerItem.obsPostId
 
-        postId.let {
-            Network.request(ApiProvider.provideTideApi().getSidePanelData(postId),
-                    NetworkCallback<SidePanelModel>().apply {
-                        success = { sidePanelModel->
-                            val tideList = sidePanelModel.data
-                            if(tideList.isNotEmpty()){
-                                event(tideList.first())
+            val postId: String ?= RealmProvider.instance.getSecondSpinnerItem(keyTide)?.obsPostId
+
+            postId?.let {
+                Network.request(ApiProvider.provideTideApi().getSidePanelData(postId),
+                        NetworkCallback<SidePanelModel>().apply {
+                            success = { sidePanelModel->
+                                val tideList = sidePanelModel.data
+                                if(tideList.isNotEmpty()){
+                                    event(tideList.first())
+                                }
                             }
-                        }
 
-                        error = {
-                            Toast.makeText(context, "데이터를 읽어오는데 실패하였습니다.\n다시 시도해주세요.", Toast.LENGTH_LONG).show()
-                        }
-                    })
-        }
+                            error = {
+                                Toast.makeText(context, "데이터를 읽어오는데 실패하였습니다.\n다시 시도해주세요.", Toast.LENGTH_LONG).show()
+                            }
+                        })
+            }
     }
 }
