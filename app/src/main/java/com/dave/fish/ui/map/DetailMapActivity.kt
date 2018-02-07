f38987d0eb1951b448ff04cc1d8003494a8e93c6
soulduse <soulduse@naver.com>
2018-02-07 23:19:52 +0900

    불필요한 코드 삭제

--- app/src/main/java/com/dave/fish/ui/map/DetailMapActivity.kt
+++ app/src/main/java/com/dave/fish/ui/map/DetailMapActivity.kt
@@ -3,8 +3,11 @@ package com.dave.fish.ui.map
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.dave.fish.R
+import com.dave.fish.db.RealmProvider
+import com.dave.fish.db.model.LocationModel
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 
 /**
@@ -22,33 +25,11 @@ class DetailMapActivity : AppCompatActivity() {
         val lon = intent.getDoubleExtra("lon", 0.0)
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map_detail) as SupportMapFragment
-        GoogleMapUtil.instance.initMap(mapFragment, lat, lon)
+        val locationList = (RealmProvider.instance.findData(LocationModel::class.java) as List<LocationModel>).last()
+        val latLonList = locationList.locations.map { LatLng(it.latitude, it.longtitude) }
+
+        GoogleMapUtil.instance
+                .initMap(mapFragment, lat, lon)
+                .initPolyLine(latLonList)
     }
-
-//    override fun onMapReady(map: GoogleMap) {
-//        mMap = map
-//
-//        val lat = intent.getDoubleExtra("lat", 0.0)
-//        val lon = intent.getDoubleExtra("lon", 0.0)
-//
-//        val mLatLng = LatLng(lat, lon)
-//
-//        mMap.setOnMyLocationButtonClickListener(this)
-//        mMap.setOnMyLocationClickListener(this)
-//        enableMyLocation()
-//        mMap.setMinZoomPreference(12.0f)
-//        mMap.setMaxZoomPreference(15.0f)
-//        mMap.addMarker(MarkerOptions().position(mLatLng).title(selectedItem.obsPostName))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(mLatLng))
-//
-//    }
-
-//    private fun enableMyLocation() {
-//        TedPermission.with(applicationContext)
-//                .setPermissionListener(permissionlistener)
-//                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
-//                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
-//                .check()
-////        }
-//    }
 }
