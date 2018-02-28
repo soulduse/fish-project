57de4fb029a30b1082d634e2ca39112c1c202e48
soulduse <soulduse@naver.com>
2018-02-28 23:28:19 +0900

    채팅 준비

---
+++ app/src/main/java/com/dave/fish/model/chat/ChatData.kt
@@ -0,0 +1,5 @@
+package com.dave.fish.model.chat
+
+/**
+ * Created by soul on 2018. 2. 28..
+ */
---
+++ app/src/main/java/com/dave/fish/ui/chat/FragmentChat.kt
@@ -0,0 +1,7 @@
+package com.dave.fish.ui.chat
+
+/**
+ * Created by soul on 2018. 2. 28..
+ */
+class FragmentChat {
+}
